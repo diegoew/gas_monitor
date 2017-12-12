245f49d4161b4427872e0baa923821bea4d60e79
Vlad Popescu <contactvlad@gmail.com>
2017-12-12 10:32:13 -0800

    First iteration of back end. We have live endpoints and a server.

---
+++ api/src/main/java/hello/GasLevelController.java
@@ -0,0 +1,31 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import io.swagger.annotations.Api;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Api(description = "These endpoints don't save data, at the moment. But they will respond" +
+        " if you call them correctly.")
+public class GasLevelController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/exampleGetValue", method = RequestMethod.GET)
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World of Gas Monitoring") String name) {
+
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+
+    }
+
+    @RequestMapping(value="/examplePostValue", method = RequestMethod.POST)
+    public String add(@RequestBody Reading input) {
+
+        return String.format(template, "buddy");
+
+    }
+}
---
+++ api/src/main/java/hello/Reading.java
@@ -0,0 +1,13 @@
+package hello;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by vladpopescu on 12/12/17.
+ */
+public class Reading {
+
+    private Map<String, Double> gasNameAndReading = new HashMap<>();
+
+}
