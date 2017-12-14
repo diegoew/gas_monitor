245fdb0b10ac7ff447861d6ea84437c74a6f63bf
kmhoran <kevin.michael.horan@gmail.com>
2017-12-14 13:07:48 -0800

    lol, uncomment the actual gas monitor code

--- gas2.py
+++ gas2.py
@@ -1,31 +1,31 @@
-#from .mq2 import MQ
+from .mq2 import MQ
 import time
 
-from gas_monitor.client.config import Config
-from gas_monitor.client.webService import WebService
+from .client.config import Config
+from .client.webService import WebService
 
 
 def run():
 
-    #try:
-    print("Press CTRL+C to abort.")
+    try:
+        print("Press CTRL+C to abort.")
 
-    config = Config()
-    
-    web = WebService(config)
+        config = Config()
 
-    #
-    print(web.getTempExample())
+        web = WebService(config)
 
-    #mq2 = MQ();
-    #while True:
-    #    perc = mq2.MQPercentage()
-    #    sys.stdout.write("\r")
-    #    sys.stdout.write("\033[K")
-    #    sys.stdout.write("CH1-LPG: %g ppm, CH2-LPG: %g ppm, CH3-LPG: %g ppm" % (perc["GAS_LPG"], perc["GAS_LPG_B"], perc["GAS_LPG_C"]))
-    #    sys.stdout.flush()
+        # Test out web service!
+        print("From web service!: ",web.getTempExample())
 
-    #time.sleep(0.1)
+        mq2 = MQ();
+        while True:
+            perc = mq2.MQPercentage()
+            sys.stdout.write("\r")
+            sys.stdout.write("\033[K")
+            sys.stdout.write("CH1-LPG: %g ppm, CH2-LPG: %g ppm, CH3-LPG: %g ppm" % (perc["GAS_LPG"], perc["GAS_LPG_B"], perc["GAS_LPG_C"]))
+            sys.stdout.flush()
 
-    #except:
-    #print("\nAbort by user")
+            time.sleep(0.1)
+
+    except:
+        print("\nAbort by user")
