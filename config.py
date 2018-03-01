2251d47d039c73adbbd9d31e8d00cbcf671af014
philip.ganchev <philip.ganchev@iamplus.com>
2018-02-28 21:53:16 -0800

    Refactor, unify scripts

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-DELAY_SECONDS = 60
-SENSOR_TYPES_WITH_INDEX = {'MQ-2': 1, 'MQ-9': 2, 'MQ-135': 3}
+REPEAT_DELAY_SECONDS = 60
+SENSOR_TYPE_TO_PIN_NUM = {'MQ-2': 1, 'MQ-9': 2, 'MQ-135': 3}
 DEVICE_ID = 'RaspPi-Prototype-1'
 LAT = '34.0376455'
 LON = '-118.437404'
 BASE_URL = 'http://54.244.200.105/readings/calculate'
 
 import logging
-logging.basicConfig(filename="gasreadings.log",
+logging.basicConfig(filename="gas_readings.log",
                     format='%(asctime)-15s %(message)s',
                     level=logging.INFO)
