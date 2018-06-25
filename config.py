dd3b958923e81f3fa663ec9fee926564f39e2d9a
philip.ganchev <philip.ganchev@iamplus.com>
2018-06-25 16:49:40 -0700

    New server IP address. Fixes for params

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ SENSOR_TYPE_TO_PIN_NUM = {'MQ-2': 1, 'MQ-9': 2, 'MQ-135': 3}
 DEVICE_ID = 'RaspPi-Prototype-1'
 LAT = '34.0376455'
 LON = '-118.437404'
-BASE_URL = 'http://54.244.200.105/readings/calculate'
+BASE_URL = 'http://34.223.248.143/readings/calculate'
 
 import logging
 logging.basicConfig(filename="gas_readings.log",
