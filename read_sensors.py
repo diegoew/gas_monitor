d403cb9182ccd3fe1e29d67f97e74faf58e27fa7
philip.ganchev <philip.ganchev@iamplus.com>
2018-08-01 15:18:05 -0700

    Separate sensor-related code into its own module

--- read_sensors.py
+++ read_sensors.py
@@ -11,12 +11,11 @@ import time
 
 import pymysql
 import requests
-import spidev
 
 from config import REPEAT_DELAY_SECONDS, SERVER_URL, DEVICE_ID, LAT, LON, \
-    SENSOR_TYPES, LOAD_RESISTANCES, AIR_RESISTANCE_RATIOS, \
-    DB_HOST, DB_USER, DB_PASSWORD, DB, DB_TABLE
+    SENSOR_TYPES, DB_HOST, DB_USER, DB_PASSWORD, DB, DB_TABLE
 import openweather
+import sensor
 
 
 DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S%z'
@@ -27,7 +26,6 @@ parser = argparse.ArgumentParser(
 )
 
 db_connection = None
-spi = spidev.SpiDev()
 
 
 def get_db_connection():
@@ -39,48 +37,6 @@ def get_db_connection():
                                         db=DB,
                                         autocommit=True)
     return db_connection
-
-
-def read_adc(pin_num):
-    """
-    :return SPI data from the MCP3008 analogue-to-digital converter (ADC),
-    from 8 channels in total.
-    """
-    assert 0 <= pin_num < len(SENSOR_TYPES), 'Invalid pin number: %s' % pin_num
-    r = spi.xfer2([1, 8 + pin_num << 4, 0])
-    return ((r[1] & 3) << 8) + r[2]
-
-
-def calibrate(pin_num):
-    """Calibrate sensor.
-    :return Ro, the calculated sensor resistance for the given sensor type
-
-    Ro as the sensor resistance at a specific calibration gas. It was defined by
-    sensor manufacturer (see the data sheet linked from the README document).
-    But because we do not have pure gases available, we approximate Ro using Rs,
-    the resistance at various concentrations of gases, and the ratio Rs/Ro which
-    is almost constant for pure air and is given by the manufacturer.
-    """
-    sensor_type = SENSOR_TYPES[pin_num]
-    print('Calibrate ' + sensor_type)
-    logging.info('Calibrate ' + sensor_type)
-
-    val = 0.0
-    for _ in range(50):
-        val += read_adc(pin_num)
-    val /= 50
-
-    if val == 0:
-        val += 0.0000001
-
-    lr = LOAD_RESISTANCES[pin_num]
-    arr = AIR_RESISTANCE_RATIOS[pin_num]
-    ro = (1023 / val - 1) * lr / arr
-
-    print('Val:', val, 'Ro:', ro)
-    logging.info('Ro=%g' % ro)
-
-    return ro
 
 
 def timestamp(dt):
@@ -152,19 +108,19 @@ def upload_recorded():
 
 
 def run():
-    spi.open(0, 0)
+    sensor.spi.open(0, 0)
 
     try:
         print('Press Ctrl+C to abort')
         logging.info('Program started')
 
-        ros = [calibrate(i) for i in range(len(SENSOR_TYPES))]
+        ros = [sensor.calibrate(i) for i in range(len(SENSOR_TYPES))]
 
         print('\nRead sensors every %s seconds...' % REPEAT_DELAY_SECONDS)
         while True:
             sys.stdout.write('\r\033[K')
             for pin_num, (sensor_type, ro) in enumerate(zip(SENSOR_TYPES, ros)):
-                val = read_adc(pin_num)
+                val = sensor.read_adc(pin_num)
                 dt = datetime.now(timezone.utc).astimezone()
                 sys.stdout.write('%s:%g ' % (sensor_type, val))
                 sys.stdout.flush()
