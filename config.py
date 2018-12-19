86066e5b083bba839835ea41b7483301c50891e4
philip.ganchev <philip.ganchev@iamplus.com>
2018-12-19 13:55:28 -0800

    Move Openweather URL to openweather.py
    It is not a config option, it is constant for OpenWeather service.

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ SENSOR_TYPES = ['MQ-2', 'MQ-9', 'MQ-135']
 LOAD_RESISTANCES = [5, 18, 20]
 AIR_RESISTANCE_RATIOS = [9.48, 9.71, 3.59]
 DB = 'measurements.sqlite'
-WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather'
 # Unique key per Gas Monitor, can be used up to 60 times per minute
 OPENWEATHER_KEY = '177f943c86860b2efb332b00962ac509'
 WEATHER_RETRIEVAL_INTERVAL_SECONDS = 60
