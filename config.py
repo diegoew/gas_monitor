44766dc2ec0a33bbf539f0cd236ca11353043709
philip.ganchev <philip.ganchev@iamplus.com>
2018-12-19 13:42:06 -0800

    Update README (instructions), config

--- config.py
+++ config.py
@@ -1,17 +1,22 @@
-DEVICE_ID = 'RaspPi-Prototype-1'
-LAT = '34.0376455'
-LON = '-118.437404'
-SERVER_URL = 'http://34.223.248.143/readings/calculate'
-REPEAT_DELAY_SECONDS = 60
-ADC_TYPE = 'ads1115'
-SENSOR_TYPES = ['MQ-2', 'MQ-9', 'MQ-135']
-LOAD_RESISTANCES = [5, 18, 20]
-AIR_RESISTANCE_RATIOS = [9.48, 9.71, 3.59]
-DB = 'measurements.sqlite'
+DEVICE_ID =   # Unique name for this device
+
+# Geographic position. You can find it using http://maps.google.com
+LAT = #34.0376455
+LON = #-118.437404
+
+ADC_TYPE = 'ads1115'  # ads1115 or mcp3008
+SENSOR_TYPES = ['MQ-2', 'MQ-9', 'MQ-135']  # Sensors connected to each pin
+LOAD_RESISTANCES = [5, 18, 20]  # Per sensor
+AIR_RESISTANCE_RATIOS = [9.48, 9.71, 3.59]  # Per sensor
+
+OPENWEATHER_KEY =  #'177f943c86860b2efb332b00962ac509'
+# From https://home.openweathermap.org/api_keys
 WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather'
-# Unique key per Gas Monitor, can be used up to 60 times per minute
-OPENWEATHER_KEY = '177f943c86860b2efb332b00962ac509'
 WEATHER_RETRIEVAL_INTERVAL_SECONDS = 60
+
+REPEAT_DELAY_SECONDS = 60
+DB = 'measurements.sqlite'
+SERVER_URL = 'http://34.223.248.143/readings/calculate'
 
 import logging
 logging.basicConfig(filename="measurements.log",
