b80f8d44e17780503453d61e7ae8f1f605dbeae3
philip.ganchev <philip.ganchev@iamplus.com>
2018-12-19 02:51:05 -0800

    Rename DB and log: simplify to "measurements"

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ SENSOR_TYPES = ['MQ-2', 'MQ-9', 'MQ-135']
 ADS1115_GAIN = 2/3  # For reading voltages from -6.144V to +6.144V.
 LOAD_RESISTANCES = [5, 18, 20]
 AIR_RESISTANCE_RATIOS = [9.48, 9.71, 3.59]
-DB = 'gas_measurements.sqlite'
+DB = 'measurements.sqlite'
 ADC_RESOLUTION = 32767  # 32767 for ADS, 1023 for MCP
 DB_TABLE = 'measurements'
 WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather'
@@ -16,7 +16,7 @@ OPENWEATHER_KEY = '177f943c86860b2efb332b00962ac509'
 WEATHER_RETRIEVAL_INTERVAL_SECONDS = 60
 
 import logging
-logging.basicConfig(filename="gas-measurements.log",
+logging.basicConfig(filename="measurements.log",
                     format='%(asctime)-15s %(message)s',
                     level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler())
